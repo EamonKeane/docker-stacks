493957b5c927f567e8744e800e4324d3ff481cd5
Eamon Keane <eamon.keane1@gmail.com>
2018-04-29 21:54:12 +0100

    added ipython-sql

--- docker-build.sh
+++ docker-build.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 DOCKERFILE=scipy-notebook/Dockerfile
-TAG=quay.io/eamonkeane/scipy-docker:1
+TAG=quay.io/eamonkeane/scipy-docker:1.2
 CONTEXT="scipy-notebook"
 
 docker build --file $DOCKERFILE --tag $TAG $CONTEXT
+docker push $TAG
+
+#docker tag quay.io/eamonkeane/scipy-docker:1 $TAG
